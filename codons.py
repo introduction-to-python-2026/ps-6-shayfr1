4ef2b84eb23e3be91967b7b1f5bbe9551b7a518d
shayfr1 <shay01fr@gmail.com>
2025-11-29 16:59:24 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -3,5 +3,5 @@ codonamino_dict = {}
     with open (file_path) as codons_file:
       codonamino_dict = {row.strip().split('\t')[0]: row.strip().split('\t')[2]
       for row in codons_file.readlines()}
-    return codonamino_dict
+return codonamino_dict
 
