66b90ad74db6543f67136fcafc95e78fa6653c02
shayfr1 <shay01fr@gmail.com>
2025-11-29 17:02:50 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,6 +1,6 @@
 def create_codon_dict(file_path):
-    codonamino_dict = {}
-     with open (file_path) as codons_file:
+ codonamino_dict = {}
+      with open (file_path) as codons_file:
       codonamino_dict = {row.strip().split('\t')[0]: row.strip().split('\t')[2]
       for row in codons_file.readlines()}
         
